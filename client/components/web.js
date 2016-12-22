7b83413a3923a1d919140cf8aecea4014295a5d9
Joosang Lee <armlessarcher@gmail.com>
2016-12-22 15:23:30 -0800

    Implement MobX Store

--- client/components/web.js
+++ client/components/web.js
@@ -1,19 +1,24 @@
 // entry point for web app
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import Store from './Store';
 
-class Web extends React.Component {
-  constructor(props){
+var Web = observer(class Web extends React.Component {
+  constructor(props) {
     super(props);
-
     this.state = {
-      test: 'Hello World'
-    }
+      test: 'Hello World',
+      test2: Store.currentUser
+    };
   }
 
-  render(){
-    return <div>{this.state.test}</div>
+  render() {
+    return <div>
+    <div>{this.state.test}</div>
+    <div>TEST2{this.state.test2} </div>
+    </div>;
   }
-}
+});
 
-ReactDOM.render(<Web />, document.getElementById('web'))
+ReactDOM.render(<Web />, document.getElementById('web'));
