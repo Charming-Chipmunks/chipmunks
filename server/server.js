6f64d4071f7a5716c60a034b249985d9a8493b19
Casey Hungler <cmhungler@gmail.com>
2016-12-22 18:58:00 -0800

    Add redirects to staging servers

--- server/server.js
+++ server/server.js
@@ -7,4 +7,25 @@ app.get('*', function(req, res) {
   res.send('Hello World');
 });
 
+
+//DEVELOPMENT CONVENIENCES
+app.get('/staging', function(req, res) {
+  res.redirect('http://jobz.mooo.com:3000');
+});
+app.get('/casey', function(req, res) {
+  res.redirect('http://jobz.mooo.com:4000');
+});
+app.get('/emm', function(req, res) {
+  res.redirect('http://jobz.mooo.com:5000');
+});
+app.get('/j', function(req, res) {
+  res.redirect('http://jobz.mooo.com:6000');
+});
+app.get('/phil', function(req, res) {
+  res.redirect('http://jobz.mooo.com:7000');
+});
+app.get('/grandmasterp', function(req, res) {
+  res.redirect('http://jobz.mooo.com:7000');
+});
+
 app.listen(process.env.PORT || 3000);
