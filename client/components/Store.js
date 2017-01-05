77fac8aaf5e4f1a4de49a57ade90a39f983b5e0f
Joosang Lee <armlessarcher@gmail.com>
2017-01-04 20:42:20 -0800

    Clear comments

--- client/components/Store.js
+++ client/components/Store.js
@@ -2,7 +2,7 @@
 import { observable } from 'mobx';
 class Store {
   constructor() {}
-  @observable currentUserId = 2;
+  @observable currentUserId = 1;
   @observable params = {
     c: false,
     python: false,
@@ -17,33 +17,8 @@ class Store {
     state: 'CA'
   };
   @observable jobList = [];
-  // @observable jobList = [];
   @observable filterText = { text: '' };
   @observable actions = [];
-  /*@observable actions = [{
-    company: 'Airbnb',
-    scheduledTime: '2016-12-25 11:30:30',
-    completedTime: '2016-12-25 11:30:30',
-    action: 'email',
-    actionType: 'userInteraction',
-    actionDetails: 'Email received from DONE',
-  }, {
-    company: 'Airbnb',
-    scheduledTime: '2017-01-11 13:30:30',
-    completedTime: null,
-    action: 'email',
-    actionType: 'recommendation',
-    actionDetails: 'Send a reply to TODO'
-  }, {
-    company: 'SomeOtherCompany',
-    scheduledTime: '2016-12-26 16:30:30',
-    completedTime: null,
-    action: 'phone',
-    actionType: 'recommendation',
-    actionDetails: 'Call a reply to OVERDUE'
-  }];
-  */
-
   @observable company = {
     name: 'Airbnb',
     location: '888 Brannan St, San Francisco, CA 94103',
@@ -56,30 +31,6 @@ class Store {
     name: 'Knox Sandy',
     email: 'knoxs@airbnb.com'
   }];
-  // @observable job = {
-  //   companyName: 'Airbnb',
-  //   positionName: 'Javascript Developer',
-  //   details: 'A leading customer engagement platform here in downtown San Francisco is looking to add a Frontend Engineer to their growing team.',
-  //   history: [{
-  //     scheduledTime: '2016-12-25 11:30:30',
-  //     completedTime: '2016-12-25 11:30:30',
-  //     action: 'email',
-  //     actionType: 'userInteraction',
-  //     actionDetails: 'Email received from DONE',
-  //   }, {
-  //     scheduledTime: '2017-01-11 13:30:30',
-  //     completedTime: null,
-  //     action: 'email',
-  //     actionType: 'recommendation',
-  //     actionDetails: 'Send a reply to TODO'
-  //   }, {
-  //     scheduledTime: '2016-12-26 16:30:30',
-  //     completedTime: null,
-  //     action: 'phone',
-  //     actionType: 'recommendation',
-  //     actionDetails: 'Call a reply to OVERDUE'
-  //   }]
-  // };
   @observable job = [];
 }
 
